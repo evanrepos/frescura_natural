2e1cd7f37146ceac5838facf0f873defad3028f9
Lucas Mamani <103885226+lucasGME@users.noreply.github.com>
2026-02-21 20:59:09 -0300

    ADD Solution

---
+++ frescura_natural/00_Creacion_DB.sql
@@ -0,0 +1,12 @@
+/*
+------------------------------------------------------------
+Universidad Nacional de La Matanza
+Trabajo Práctico Integrador - Bases de Datos Aplicadas
+Integrantes: 
+Apellido y Nombre						
+Gonzáles Fernándes Iván Alejandro		
+Juan Bautista Sabaris					
+Mamani Estrada Lucas Gabriel			
+
+------------------------------------------------------------
+*/
