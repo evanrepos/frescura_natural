a69b21993b684314bcb844f77b94b2d49ad73e8c
Lucas Mamani <103885226+lucasGME@users.noreply.github.com>
2026-02-22 20:18:01 -0300

    Updata 00_Creacion_DB
    creacion de tablas y esquemas

--- frescura_natural/00_Creacion_DB.sql
+++ frescura_natural/00_Creacion_DB.sql
@@ -35,34 +35,187 @@ GO
 ----------------------------------------------------------------
 -- CREACIÓN DE ESQUEMAS
 ----------------------------------------------------------------
-CREATE SCHEMA esquema_1;
+CREATE SCHEMA productos;     
 GO
-CREATE SCHEMA esquema_2;
+CREATE SCHEMA proveedores; 
 GO
-CREATE SCHEMA esquema_4;
+CREATE SCHEMA sucursales;  
 GO
-CREATE SCHEMA esquema_5;
+CREATE SCHEMA ventas;     
 GO
 
 PRINT 'Esquemas creados correctamente';
 GO
+----------------------------------------------------------------
+-- ESQUEMA: sucursales (sucursal, capacitador, vendedor)
+----------------------------------------------------------------
+CREATE TABLE sucursales.sucursal (
+    id_sucursal INT IDENTITY(1,1) PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    direccion VARCHAR(100) NOT NULL,
+    localidad VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE sucursales.capacitador (
+    id_capacitador INT IDENTITY(1,1) PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE sucursales.vendedor (
+    id_vendedor INT IDENTITY(1,1) PRIMARY KEY,
+    id_capacitador INT NOT NULL,
+    id_sucursal INT NOT NULL,
+    nombre VARCHAR(50) NOT NULL,
+    fecha_capacitacion DATE,
+    CONSTRAINT FK_vendedor_capacitador FOREIGN KEY (id_capacitador) 
+		REFERENCES sucursales.capacitador (id_capacitador),
+    CONSTRAINT FK_vendedor_sucursal FOREIGN KEY (id_sucursal)
+		REFERENCES sucursales.Sucursal (id_sucursal)
+);
+GO
+----------------------------------------------------------------
+-- ESQUEMA: proveedores (proveedor)
+----------------------------------------------------------------
+
+CREATE TABLE proveedores.proveedor (
+    id_proveedor INT IDENTITY(1,1) PRIMARY KEY,
+    nombre VARCHAR(30) NOT NULL,
+    direccion VARCHAR(100),
+    telefono VARCHAR(20)
+);
+GO
 
 ----------------------------------------------------------------
--- TABLAS DE ESQUEMA_1
+-- ESQUEMA: productos (categoria, temporada, producto)
 ----------------------------------------------------------------
 
-----------------------------------------------------------------
--- TABLAS DE ESQUEMA_2
-----------------------------------------------------------------
+CREATE TABLE productos.categoria (
+    id_categoria INT IDENTITY(1,1) PRIMARY KEY,
+    descripcion VARCHAR(50) NOT NULL
+);
+
+
+CREATE TABLE productos.temporada (
+    id_temporada INT IDENTITY(1,1) PRIMARY KEY,
+    descripcion VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE productos.producto (
+    id_producto INT IDENTITY(1,1) PRIMARY KEY,
+    id_proveedor INT NOT NULL,
+    id_categoria INT NOT NULL,
+    id_temporada INT NOT NULL,
+    descripcion VARCHAR(50) NOT NULL,
+    precio DECIMAL(8,2) NOT NULL,
+	CONSTRAINT CK_producto_precio CHECK (precio >= 0),
+    CONSTRAINT FK_Producto_Proveedor FOREIGN KEY (id_proveedor) 
+		REFERENCES proveedores.proveedor (id_proveedor),
+    CONSTRAINT FK_producto_categoria FOREIGN KEY (id_categoria) 
+		REFERENCES productos.categoria (id_categoria),
+    CONSTRAINT FK_producto_temporada FOREIGN KEY (id_temporada) 
+		REFERENCES productos.temporada (id_temporada)
+);
+GO
 
 ----------------------------------------------------------------
--- TABLAS DE ESQUEMA_3
+-- ESQUEMA: proveedores (ingreso, lote, lineaIngreso)
 ----------------------------------------------------------------
 
-----------------------------------------------------------------
--- TABLAS DE ESQUEMA_4
-----------------------------------------------------------------
+CREATE TABLE proveedores.ingreso (
+    id_ingreso INT IDENTITY(1,1) PRIMARY KEY,
+    id_proveedor INT NOT NULL,
+    id_sucursal INT NOT NULL,
+    fecha_hora DATETIME NOT NULL,--capaz  DEFAULT GETDATE()
+    CONSTRAINT FK_ingreso_proveedor FOREIGN KEY (id_proveedor)
+        REFERENCES proveedores.proveedor (id_proveedor),
+    CONSTRAINT FK_ingreso_sucursal  FOREIGN KEY (id_sucursal)
+        REFERENCES sucursales.sucursal (id_sucursal)
+);
+
+CREATE TABLE proveedores.lote (
+    numero INT IDENTITY(1,1) PRIMARY KEY,--capaz sin IDENTITY o con un id con identity y otro campo con numero
+    id_producto INT NOT NULL,
+    id_ingreso INT NOT NULL,
+    fecha_ingreso DATE NOT NULL,
+    CONSTRAINT FK_lote_producto FOREIGN KEY (id_producto)
+        REFERENCES productos.producto (id_producto),
+    CONSTRAINT FK_lote_ingreso  FOREIGN KEY (id_ingreso)
+        REFERENCES proveedores.ingreso (id_ingreso)
+);
+
+CREATE TABLE proveedores.lineaIngreso (
+    numero INT IDENTITY(1,1) NOT NULL PRIMARY KEY,--capaz sin IDENTITY o con un id con identity y otro campo con numero
+    numero_lote INT NOT NULL,
+    cantidad SMALLINT NOT NULL,
+    CONSTRAINT CK_lineaIngreso_cantidad CHECK (cantidad > 0),
+    CONSTRAINT FK_lineaIngreso_lote FOREIGN KEY (numero_lote)
+        REFERENCES proveedores.lote (numero)
+);
+GO
 
 ----------------------------------------------------------------
--- TABLAS DE ESQUEMA_5
+-- ESQUEMA: ventas (cliente, pedido, venta, lineaVenta)
 ----------------------------------------------------------------
+
+CREATE TABLE ventas.cliente (
+    id_cliente INT IDENTITY(1,1) PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    direccion VARCHAR(100) NOT NULL,
+    cuit_cuil CHAR(11) NULL,
+	CONSTRAINT CK_cliente_cUIT_Formato CHECK (
+        cuit_cuil IS NULL OR (LEN(cuit_cuil) = 11 AND cuit_cuil NOT LIKE '%[^0-9]%'))
+);
+
+CREATE TABLE ventas.pedido (--revisar
+    id_pedido INT IDENTITY(1,1) PRIMARY KEY,
+    id_cliente INT NOT NULL,
+    fecha DATE NOT NULL,
+    CONSTRAINT FK_pedido_cliente FOREIGN KEY (id_cliente)--esta bien?
+        REFERENCES ventas.cliente (id_cliente)
+);
+
+CREATE TABLE ventas.venta (--falta agregar la fk de pedido??
+    id_venta INT IDENTITY(1,1) PRIMARY KEY,
+    id_cliente INT NOT NULL,
+    id_vendedor INT NOT NULL,
+    fecha_hora DATETIME NOT NULL,
+    total DECIMAL(9,2) NOT NULL,
+    CONSTRAINT CK_venta_total CHECK (total > 0),
+    CONSTRAINT FK_venta_cliente FOREIGN KEY (id_cliente)
+        REFERENCES ventas.cliente (id_cliente),
+    CONSTRAINT FK_venta_vendedor FOREIGN KEY (id_vendedor)
+        REFERENCES sucursales.vendedor (id_vendedor)
+);
+
+CREATE TABLE ventas.lineaVenta (
+    id_lineaVenta INT IDENTITY(1,1) PRIMARY KEY,
+    id_venta INT NOT NULL,
+    id_producto INT NOT NULL,
+    cantidad SMALLINT NOT NULL,
+    CONSTRAINT CK_lineaVenta_cantidad CHECK (cantidad > 0),
+    CONSTRAINT FK_lineaVenta_venta FOREIGN KEY (id_venta)
+        REFERENCES ventas.venta (id_venta),
+    CONSTRAINT FK_lineaVenta_producto FOREIGN KEY (id_producto)
+        REFERENCES productos.producto (id_producto)
+);
+GO
+
+----------------------------------------------------------------
+-- ESQUEMA: proveedores (precio)
+----------------------------------------------------------------
+
+CREATE TABLE proveedores.precio (
+    id_precio INT IDENTITY(1,1) PRIMARY KEY,
+    id_producto INT NOT NULL,
+    id_proveedor INT NOT NULL,
+    monto DECIMAL(8,2) NOT NULL,
+    CONSTRAINT CK_precio_monto CHECK (monto > 0),
+    CONSTRAINT FK_precio_producto FOREIGN KEY (id_producto)
+        REFERENCES productos.producto (id_producto),
+    CONSTRAINT FK_precio_proveedor FOREIGN KEY (id_proveedor)
+        REFERENCES proveedores.proveedor (id_proveedor)
+);
+GO
+
+PRINT 'Tablas creadas correctamente';
+GO
