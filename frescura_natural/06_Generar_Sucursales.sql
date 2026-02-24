437f7c9966c2c0429c5ca52645f1ef6a763b6cac
evanrepos <ivanagf04@gmail.com>
2026-02-24 00:57:03 -0300

    Terminado el script de generación de sucursales.

--- frescura_natural/06_Generar_Sucursales.sql
+++ frescura_natural/06_Generar_Sucursales.sql
@@ -2,35 +2,15 @@ USE FrescuraNatural
 GO
 
 CREATE OR ALTER PROCEDURE sucursales.sp_ingresar_sucursales 
-    @path VARCHAR(100),
-    @page_name VARCHAR(30),
-    @row_terminator CHAR(2) = '\n',
-    @field_terminator CHAR(1) = ','
 AS
 BEGIN
     SET NOCOUNT ON 
-    CREATE TABLE #merma(
-        fecha DATETIME,
-        producto VARCHAR(20),
-        cantidad TINYINT,
-        sucursal VARCHAR(15)
-    );
-    
-    DECLARE @openrowset VARCHAR(MAX);
-    SET @openrowset = 'INSERT INTO #merma
-        SELECT * FROM OPENROWSET(
-    ''Microsoft.ACE.OLEDB.16.0'',
-    ''Excel 12.0;HDR=YES;IMEX=1;Database=' + @path +''',
-    ''SELECT * FROM [' + @page_name +'$]''
-    );';
-    EXEC (@openrowset);
-    
-    INSERT INTO sucursales.sucursal (nombre, direccion, localidad)
-        SELECT DISTINCT 'nombre', 'direccion', sucursal FROM #merma;
+    INSERT INTO sucursales.sucursal (localidad)
+        SELECT DISTINCT sucursal FROM datos.mermas
 END
 GO
 
   --REEMPLAZAR EL CAMPO <ruta> POR EL PATH DEL ARCHIVO.
-EXEC sucursales.sp_ingresar_sucursales <ruta>, 'desperdicios'
+EXEC sucursales.sp_ingresar_sucursales
 GO
 SELECT * FROM sucursales.sucursal
